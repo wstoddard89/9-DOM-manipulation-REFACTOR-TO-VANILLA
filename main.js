e42e3395c1707c0ff641745502926b7af8e86b91
Will Stoddard <wstoddard89@gmail.com>
2020-09-08 07:24:52 -0700

    rework and finish html and javascsript

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
-function showText1() {
-  document.querySelector("#par1").style.display = "block";
-  document.querySelector("#par2").style.display = "none";
-  document.querySelector("#par3").style.display = "none";
+
+//document.addEventListener('DOMContentLoaded', function() {
+
+
+
+// const accordionItemHeader = document.querySelectorAll(".accordion-item-header");
+
+const accordionItems = document.querySelectorAll(".accordion-item")
+
+
+for (let i = 0; i < accordionItems.length; i++) {
+  let element = accordionItems[i]
+  element.addEventListener('click', function(e) {
+    const accordionContent = document.querySelectorAll(".accordion-item-pgraph");
+    for (let i = 0; i < accordionContent.length; i++) {
+      
+      let element = accordionContent[i]
+      element.classList.remove('active')
+  }
+
+    e.preventDefault()
+    const currentContent = element.querySelectorAll(".accordion-item-pgraph")[0]
+    currentContent.classList.add('active')
+    
+  }) 
 }
 
-function showText2() {
-  document.querySelector("#par2").style.display = "block";
-  document.querySelector("#par1").style.display = "none";
-  document.querySelector("#par3").style.display = "none";
-}
 
-function showText3() {
-  document.querySelector("#par3").style.display = "block";
-  document.querySelector("#par1").style.display = "none";
-  document.querySelector("#par2").style.display = "none";
-}
 
-// function showSlowly1() {
-//   document.querySelector('#par1').toggleClass = "showSlowly1";
+// function toggle(event) {
+//  for (var i = 0; i < accordionContent.length; i++) {
+// console.log(accordionContent[i]);
+// let element = accordionContent[i]
+// element.classList.remove('accordion-item-pgraph')
+//  }
 // }
 
-// let par1 = document.querySelector("#par1");
-// let heading1 = document.querySelector("#heading1")
-
-// heading1.addEventListener('click', function() {
-//   par1.className = par1.className !== 'show' ? 'show' : 'hide';
-//   if (par1.className === 'show') {
-//     par1.style.display = 'block';
-//     window.setTimeout(function(){
-//       par1.style.opacity = 1;
-//       par1.style.transform = 'scale(1)';
-//     },0);
-//   }
-//   if (par1.className === 'hide') {
-//     par1.style.opacity = 0;
-//     par1.style.transform = 'scale(0)';
-//     window.setTimeout(function(){
-//       par1.style.display = 'none';
-//     },700); // timed to match animation-duration
-//   }
-// })
+//})
